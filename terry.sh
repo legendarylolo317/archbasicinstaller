06efd0db7b5778b464304138d097d26824bb27e7
legendarylolo317 <71846249+legendarylolo317@users.noreply.github.com>
2022-10-04 21:42:50 -0700

    Delete terry.sh

--- terry.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-umount -R /mnt
-echo -e "The system is now going to reboot after this is done you can delete this file and the archinstall.sh file from your directory."
-sleep 10s
-shutdown now
