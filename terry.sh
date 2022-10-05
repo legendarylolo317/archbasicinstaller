6969803f73c3c0910b040c4561fc57c57b149924
legendarylolo317 <71846249+legendarylolo317@users.noreply.github.com>
2022-10-05 14:03:10 -0700

    Delete terry.sh

--- terry.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-umount -R /mnt
-echo -e "The system is now going to reboot after this is done you can delete this file and the archinstall.sh file from your directory."
-sleep 10s
-rm /terry.sh
-rm /archinstall.sh
-rm /yes.sh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-shutdown now
