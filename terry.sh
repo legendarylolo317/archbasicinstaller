160123d18aac8ae811778f91e51c4e2ae2689864
legendarylolo317 <71846249+legendarylolo317@users.noreply.github.com>
2022-10-04 21:26:37 -0700

    Add files via upload

---
+++ terry.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+umount -R /mnt
+echo -e "The system is now going to reboot after this is done you can delete this file and the archinstall.sh file from your directory."
+sleep 10s
+shutdown now
